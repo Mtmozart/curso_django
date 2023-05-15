9e9654200995ba59f1b7e81631e0d177318ee1e4
TheSamuraiBr <mmsnborges@gmail.com>
2023-05-15 12:56:58 -0300

    I had used the shell and create logic for user

--- core/views.py
+++ core/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 
 def index(request):
+    print(dir(request.user))
+    print(f"User: {request.user}")
+
+    if str(request.user) == 'AnonymousUser':
+        test = 'Usuário não logado'
+    else:
+        test = 'Usuário logado'
     context = {
         'curso': 'Apredendo progamação web com django framework',
-        'outro': 'Esse aqui parece ser muito legal'
+        'outro': 'Esse aqui parece ser muito legal',
+        'logado': test
     }
     return render(request, 'index.html', context)
 
