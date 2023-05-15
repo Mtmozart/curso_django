14363119631ad639b08a1b9a8e2d9ce9e1bcdec3
TheSamuraiBr <mmsnborges@gmail.com>
2023-05-15 09:53:24 -0300

    models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here - comando para execultar a migration python menage.py makemigration e migrate para execultar.
+
+class Product(models.Model):
+    name = models.CharField('Nome', max_length=100)
+    price = models.DecimalField('Preço', decimal_places=2, max_digits=8)
+    stock = models.IntegerField('Quantidade em estoque')
+
+class Client(models.Model):
+    name = models.CharField('Nome', max_length=20)
+    last_name = models.CharField('Sobrenome', max_length=20)
+    email = models.EmailField('E-mail', max_length=100)
+
