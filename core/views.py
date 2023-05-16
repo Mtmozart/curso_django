97fd2958fdcf0761393996c9107b8fed4e55a22d
TheSamuraiBr <mmsnborges@gmail.com>
2023-05-15 21:26:40 -0300

    Travail avec le banque et template

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import Product
 def index(request):
+    products = Product.objects.all()
     print(dir(request.user))
     print(f"User: {request.user}")
 
@@ -11,10 +12,18 @@ def index(request):
     context = {
         'curso': 'Apredendo progamação web com django framework',
         'outro': 'Esse aqui parece ser muito legal',
+        'products': products,
         'logado': test
     }
     return render(request, 'index.html', context)
 
 def contact(request):
     return render(request, 'contact.html')
+
+def product(request, pk):
+    prod = Product.objects.get(id=pk)
+    context = {
+        'product': prod
+    }
+    return render(request, 'product.html', context)
 
